0a9bf6e0b2245f236855344e3ec7f742cf8048c2
John Larson <jl@buildmidwestern.com>
2024-06-14 10:08:42 -0500

    fixed syntax

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 )
 
 func main () {
-	fmt. Println("hello world" )
+	fmt.Println("hello world")
 	
-	godotenv. Load ( )
+	godotenv.Load()
 	
-	portString := os. Getenv ("PORT" )
-	if portString == "" {
-		log.Fatal("'PORT is not found in the environment")
+	portString := os. Getenv("PORT")
+	if portString == ""{
+		log.Fatal("PORT is not found in the environment")
 	}
 	
 	fmt. Println("Port:", portString)
